c29de7604f89c2808dd86eae94a188ff18c4843e
Mojan Jamalzadeh <mojanjamalzadeh@yahoo.com>
2021-03-10 05:06:03 +0100

    nr tasklet script fix

--- orc-parser/nr_tasklet_exp.sh
+++ orc-parser/nr_tasklet_exp.sh
@@ -1,22 +1,16 @@
 #!/bin/bash
 
 
-tasklets_set="1 2 4 6 8 10 12 14 16 18 20 22"
-file = "/home/tpcds-data-orc/scale=10-parts=1/part-00000-7cd0e244-020a-4d11-9b12-53eb404beac0-c000.snappy.orc"
-
-#tasklets_set="14 16"
-
-
-
-
-
+tasklets_set="8 6 4 3 2 1"
+file="/home/upmem0016/mojanjamalzadeh/tpcds-data-orc/scale=10-parts=1/part-00000-7cd0e244-020a-4d11-9b12-53eb404beac0-c000.snappy.orc"
 for tasklets in $tasklets_set; do
     # remove the dpu binary
     rm ../snappy/pim-snappy/decompress.dpu
     # compile with the given number of tasklets
-    make USE_PIM=1 NR_TASKLETS=tasklets
-    # run and store runtime 
-    {echo tasklets; time ./reader -f file -t 8} 2>time.txt
+    make USE_PIM=1 NR_TASKLETS=$tasklets
+    # run and store runtime
+   echo "Tasklets: $tasklets"
+   time ./reader -f ../testfiles/customer-scale1000-parts1.snappy.orc -t 100 
 done
 
 
